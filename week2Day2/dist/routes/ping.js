00a83eb6ad4d6b6d7c091c64886a837cfbf445fc
AhmadNajiKam <ahmadnajikam@gmail.com>
2025-10-23 05:36:54 +0300

    Remove node_modules and dist folder from the repo

--- week2Day2/dist/routes/ping.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-export const pingRouter = Router();
-// GET /api/ping - Returns "pong"
-pingRouter.get('/ping', (req, res) => {
-    res.status(200).json({
-        message: 'pong',
-    });
-});
-//# sourceMappingURL=ping.js.map
